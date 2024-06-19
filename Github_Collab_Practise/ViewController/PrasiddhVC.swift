ac52bb4f368b542f175cac3f4766d4c50040a84a
prasiddhbarot24 <prasiddhrao12345@gmail.com>
2024-06-19 18:51:47 +0530

    Prasiddh's Story Board set....

---
+++ Github_Collab_Practise/ViewController/PrasiddhVC.swift
@@ -0,0 +1,29 @@
+//
+//  PrasiddhVC.swift
+//  Github_Collab_Practise
+//
+//  Created by webcodegenie on 19/06/24.
+//
+
+import UIKit
+
+class PrasiddhVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

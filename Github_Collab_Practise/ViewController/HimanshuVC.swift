39b6b1918034d5549486384e358d94b0fd6471fd
Himanshu1110 <himanshufulwani1990@gmail.com>
2024-06-19 18:56:26 +0530

    Himanshu branch added

---
+++ Github_Collab_Practise/ViewController/HimanshuVC.swift
@@ -0,0 +1,22 @@
+//
+//  HimanshuVC.swift
+//  Github_Collab_Practise
+//
+//  Created by Webcodegenie on 19/06/24.
+//
+
+import UIKit
+
+class HimanshuVC: UIViewController {
+    @IBOutlet weak var lblName: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        lblName.text = "Himanshu"
+    }
+    
+
+
+}
